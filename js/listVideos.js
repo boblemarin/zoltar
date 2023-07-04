af330f09cc23091d7c99de01e046b9bc3f1d1d56
boblemarin <emeric@minimal.be>
2023-07-04 16:03:31 +0200

    listign multinple intro videos

--- js/listVideos.js
+++ js/listVideos.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
-
 const sep = "\\";// "/";//
-
 const currentDir = __dirname;//"E:/Expos/KERMESSE/DEV/Zoltar/html/";
-const videoDir = sep+".."+sep+"video"+sep+"reponses"+sep;
 
-console.log(currentDir + videoDir);
 
-let videos = {};
 
-subDirs = fs.readdirSync(currentDir + videoDir, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
-  
+// LIST REPONSES
+const reponsesDir = sep+".."+sep+"video"+sep+"reponses"+sep;
+let reponses = {};
+let subDirs = fs.readdirSync(currentDir + reponsesDir, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
 subDirs.forEach(file => {
-	let curDir = currentDir + videoDir + file.name;
+	let curDir = currentDir + reponsesDir + file.name;
 	let files = fs.readdirSync(curDir, { withFileTypes: true })
 	let array = [];
 	files.forEach(file => {
 		//console.log(file.name);
 		array.push(file.name);
 	});
-	videos[file.name] = array;
+	reponses[file.name] = array;
 });
 
-let output = JSON.stringify(videos);
+// LIST INTROS
+const introsDir = sep+".."+sep+"video"+sep+"intros"+sep;
+let intros  = [];
+let introsFiles = fs.readdirSync(currentDir + introsDir, { withFileTypes: true });
+introsFiles.forEach(file => {
+	intros.push(file.name);
+});
 
-fs.writeFileSync(currentDir + sep + "videos.js", "let videoFiles = " + output + ";");
+
+fs.writeFileSync(currentDir + sep + "videos.js", 'let introFiles = ["' + intros.join('","') + '"];\nlet videoFiles = ' + JSON.stringify(reponses) + ";");
 
 
 console.log("Voilà c'est fait...");
